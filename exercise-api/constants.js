da145b537987854daebcd4d0d74e848da053d7eb
vaishkothari-22 <152173499+vaishkothari-22@users.noreply.github.com>
2023-12-16 23:38:56 +0530

    Delete exercise-api directory

--- exercise-api/constants.js
+++
@@ -1,5 +0,0 @@
-export const ERROR_MESSAGE="Something went wrong";
-export const INSERT_SUCCESS="Data inserted";
-export const STUEDNT_NOT_FOUND="Log not found";
-export const UPDATE_SUCCESS="Log updated";
-export const DELETE_SUCCESS="Log deleted";
